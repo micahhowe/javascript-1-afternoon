51efb1f4e68197b96137ac2b2ebfee564a958a08
micahhowe <micahhowe182@gmail.com>
2019-06-25 13:44:50 -0600

    first push

--- practice.js
+++ practice.js
@@ -3,13 +3,13 @@
 //Create a variable called myName that is a string data type
 
 //Code here
-
+var myName = `string`
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
 //Code here
-
+var myAge = 31
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
